62a604b58d4c78c28091af9db7f5e0413e9b4e1a
lebrancconvas <lebrancconvas@gmail.com>
2023-09-03 08:00:48 +0700

    ✨ feat: circle component

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { canvas, ctx, config } from './canvas';
+import { Circle } from './components';
 
 // Prepare the canvas.
 canvas.width = config.width;
@@ -6,3 +7,10 @@ canvas.height = config.height;
 canvas.style.backgroundColor = config.backgroundColor;
 
 // Code.
+const centerPoint = {
+  x: canvas.width / 2,
+  y: canvas.height / 2
+};
+
+const circle = new Circle(centerPoint.x, centerPoint.y, 30, 'Yellow');
+circle.draw(ctx); 
